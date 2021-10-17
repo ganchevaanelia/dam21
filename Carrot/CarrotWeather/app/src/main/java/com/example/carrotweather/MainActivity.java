d65a72f671d86b192a037ae4526ef9423fff40c5
ganchevaanelia <72299227+ganchevaanelia@users.noreply.github.com>
2021-10-17 15:50:21 +0300

    tema1 - carrot weather login si register

---
+++ Carrot/CarrotWeather/app/src/main/java/com/example/carrotweather/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.carrotweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(register);
+            }
+        });
+    }
+}
