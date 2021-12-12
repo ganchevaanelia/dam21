ab9ab4b43b8b44f281ddab8ce0009ec3114acd98
ganchevaanelia <ganchevaanelia18@stud.ase.ro>
2021-12-12 02:36:29 +0200

    firebase

--- app/src/main/java/com/example/carrot/LoginActivity.java
+++ app/src/main/java/com/example/carrot/LoginActivity.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -59,10 +66,43 @@ public class LoginActivity extends AppCompatActivity {
 
                             userDao = Database.getInstance(LoginActivity.this).getDatabase().userDao();
                             userDao.insert(user);
+                            writeToDatabase(user);
+                            readFromDatabase(user);
 
 
                 }
             }
         }
+    }
+
+    private void writeToDatabase(User user){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(user.getUsername());
+
+        myRef.child("Email").setValue(user.getEmail());
+        myRef.child("Username").setValue(user.getUsername());
+        myRef.child("Password").setValue(user.getPassword());
+    }
+
+    private void readFromDatabase(User user){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(user.getUsername());
+// Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                User value = dataSnapshot.getValue(User.class);
+
+                Log.d("readFirebase", "User is: " + value.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("cancelled", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
